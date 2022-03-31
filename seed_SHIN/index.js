fd1ea32745300dd814659f2f8c4f4a03f593b8f3
shinchaeyoun <100834190+shinchaeyoun@users.noreply.github.com>
2022-03-31 18:40:45 +0900

    Add files via upload

--- seed_SHIN/index.js
+++ seed_SHIN/index.js
@@ -1,4 +1,48 @@
 $(function(){
 
-  const imgArr1 = new Array();
+  const imgArr = new Array();
+  imgArr[0] = 'https://shinchaeyoun.github.io/shin/seed_SHIN/img/img1.JPG';
+  imgArr[1] = 'https://shinchaeyoun.github.io/shin/seed_SHIN/img/img2.JPG';
+  imgArr[2] = 'https://shinchaeyoun.github.io/shin/seed_SHIN/img/img3.JPG';
+  imgArr[3] = 'https://shinchaeyoun.github.io/shin/seed_SHIN/img/img4.JPG';
+  imgArr[4] = 'https://shinchaeyoun.github.io/shin/seed_SHIN/img/img5.JPG';
+  imgArr[5] = 'https://shinchaeyoun.github.io/shin/seed_SHIN/img/img6.JPG';
+  imgArr[6] = 'https://shinchaeyoun.github.io/shin/seed_SHIN/img/img7.JPG';
+  imgArr[7] = 'https://shinchaeyoun.github.io/shin/seed_SHIN/img/img8.JPG';
+  imgArr[8] = 'https://shinchaeyoun.github.io/shin/seed_SHIN/img/img9.JPG';
+  imgArr[9] = 'https://shinchaeyoun.github.io/shin/seed_SHIN/img/img10.JPG';
+  imgArr[10] = 'https://shinchaeyoun.github.io/shin/seed_SHIN/img/img11.JPG';
+
+
+  // function showImg(){
+  $(window).load(function(){
+    const showImg = function(){
+      const $imgNum = Math.round(Math.random()*10);
+      let $objImg1 = $('#introImg');
+      let $objImg2 = $('#blurImg');
+
+      $objImg1.attr('src',imgArr[$imgNum]);
+      $objImg2.attr('src',imgArr[$imgNum]);
+    };
+
+    showImg();
+  });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
