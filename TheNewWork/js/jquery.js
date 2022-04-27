9ce79611f89d035a785185e3bc6009be5ea99908
shinchaeyoun <100834190+shinchaeyoun@users.noreply.github.com>
2022-04-27 10:50:47 +0900

    Add files via upload

--- TheNewWork/js/jquery.js
+++ TheNewWork/js/jquery.js
@@ -106,64 +106,131 @@ $(function(){
 
   // 어바웃미 이미지 영역 트리거
 const $aboutme =  $('#about_me');
+const $frameWid = $('.frame_wrap').children('li').width();
 const $main =  $('#main');
 const $mainHei =  $main.offsetTop;
 
-  ScrollTrigger.name = "ScrollTrigger"; 
-  gsap.registerPlugin(ScrollTrigger);
+$(window).on('resize load', function (){
 
-  let imgBox = gsap.timeline({ 
-      scrollTrigger: {
-          trigger: '#about_me', 
-          pin: true, 
-          start: $main.$mainHei, 
-          end: $aboutme.offsetWidth, 
-          scrub: 1,  
-          ease: "power3",
-          markers:true 
-      }
-  });
+  // 윈도우의 사이즈가 480px보다 큰 경우
+  if(window.innerWidth >= 480) {
+    console.log('480보다 큼');
+    ScrollTrigger.name = "ScrollTrigger"; 
+    gsap.registerPlugin(ScrollTrigger);
+    
+    let imgBox = gsap.timeline({ 
+        scrollTrigger: {
+            trigger: '#about_me', 
+            pin: true, 
+            start: $main.$mainHei, 
+            end: $aboutme.offsetWidth, 
+            scrub: 1,  
+            ease: "power3",
+            markers:true 
+        }
+    });
+    
+    imgBox.to(".black", { 
+      opacity:0
+    },0);
+    imgBox.to(".frame_wrap", { 
+      rotation:0,x:-$frameWid ,y:0
+    },2);
+    imgBox.to(".frame_wrap", { 
+      rotation:0,x:-$frameWid*2 ,y:0
+    },4);
+    imgBox.to(".frame_wrap", { 
+      rotation:0,x:-$frameWid*3 ,y:0
+    },6);
+    imgBox.to(".black", { 
+      opacity:1
+    },8);
+    
+    
+    let textBox = gsap.timeline({ 
+        scrollTrigger: {
+            trigger: '#about_me', 
+            scrub: 1,  
+            pin: true, 
+            start: $main.$mainHei, 
+            end: $aboutme.offsetWidth, 
+            ease: "power3", 
+            markers:false 
+        }
+    });
+    textBox.to(".text_wrap", { 
+        rotation:0,x:0 ,y:-480
+    },2); 
+    textBox.to(".text_wrap", { 
+        rotation:0,x:0 ,y:-960
+    },4); 
+    textBox.to(".text_wrap", { 
+        rotation:0,x:0 ,y:-1440
+    },6); 
+    textBox.to(".text_wrap", { 
+        rotation:0,x:0 ,y:-1440
+    },8); 
+  } else {
+    console.log('480보다 작음');
+    ScrollTrigger.name = "ScrollTrigger"; 
+    gsap.registerPlugin(ScrollTrigger);
+    
+    let imgBox = gsap.timeline({ 
+        scrollTrigger: {
+            trigger: '#about_me', 
+            pin: true, 
+            start: $main.$mainHei, 
+            end: $aboutme.offsetWidth, 
+            scrub: 1,  
+            ease: "power3",
+            markers:true 
+        }
+    });
+    
+    imgBox.to(".black", { 
+      opacity:0
+    },0);
+    imgBox.to(".frame_wrap", { 
+      rotation:0,x:-$frameWid ,y:0
+    },2);
+    imgBox.to(".frame_wrap", { 
+      rotation:0,x:-$frameWid*2 ,y:0
+    },4);
+    imgBox.to(".frame_wrap", { 
+      rotation:0,x:-$frameWid*3 ,y:0
+    },6);
+    imgBox.to(".black", { 
+      opacity:1
+    },8);
+    
+    console.log($frameWid);
+    
+    let textBox = gsap.timeline({ 
+        scrollTrigger: {
+            trigger: '#about_me', 
+            scrub: 1,  
+            pin: true, 
+            start: $main.$mainHei, 
+            end: $aboutme.offsetWidth, 
+            ease: "power3", 
+            markers:false 
+        }
+    });
+    textBox.to(".text_wrap", { 
+        rotation:0,x:0 ,y:-400
+    },2); 
+    textBox.to(".text_wrap", { 
+        rotation:0,x:0 ,y:-800
+    },4); 
+    textBox.to(".text_wrap", { 
+        rotation:0,x:0 ,y:-1200
+    },6); 
+    textBox.to(".text_wrap", { 
+        rotation:0,x:0 ,y:-1200
+    },8); 
+  }
+}); // resize load end
 
-  imgBox.to(".black", { 
-    opacity:0
-  },0);
-  imgBox.to(".frame_wrap", { 
-    rotation:0,x:-530 ,y:0
-  },2);
-  imgBox.to(".frame_wrap", { 
-    rotation:0,x:-1060 ,y:0
-  },4);
-  imgBox.to(".frame_wrap", { 
-    rotation:0,x:-1590 ,y:0
-  },6);
-  imgBox.to(".black", { 
-    opacity:1
-  },8);
-
-
-  let textBox = gsap.timeline({ 
-      scrollTrigger: {
-          trigger: '#about_me', 
-          scrub: 1,  
-          pin: true, 
-          start: $main.$mainHei, 
-          end: $aboutme.offsetWidth, 
-          ease: "power3", 
-          markers:false 
-      }
-  });
-  textBox.to(".text_wrap", { 
-      rotation:0,x:0 ,y:-480
-  },2); 
-  textBox.to(".text_wrap", { 
-      rotation:0,x:0 ,y:-960
-  },4); 
-  textBox.to(".text_wrap", { 
-      rotation:0,x:0 ,y:-1440
-  },6); 
-  textBox.to(".text_wrap", { 
-      rotation:0,x:0 ,y:-1440
-  },8); 
 
 
 
