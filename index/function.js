b362f416263dcb2c466d93b2f4219a092a960ae1
shinchaeyoun <100834190+shinchaeyoun@users.noreply.github.com>
2022-08-12 19:03:18 +0900

    Add files via upload

--- index/function.js
+++ index/function.js
@@ -4,18 +4,32 @@ $(function(){
     let gnbIdx = null;
 
     for(let i=0; i<$gnb.length; i++){
-        arrTop[i] = $('section').eq(i).offset();
+        arrTop[i] = $('section').eq(i).offset().top;
     };
 
 
     $gnb.click(function(e){
-        e.preventDefault();
+        //e.preventDefault();
 
         gnbIdx = $gnb.index(this);
 
+        //$(this).parent().addClass('active').siblings().removeClass('active')
+
         $('html,body').stop().animate({
-            scrollTop : arrTop[gnbIdx+1].top
+            scrollTop : arrTop[gnbIdx]
         });
+    });
+
+    $(window).scroll(function(){
+        const scrollTop = $(this).scrollTop();
+
+        for(let i=0; i<arrTop.length;i++){
+            if(scrollTop >= arrTop[i] -100 ){
+                $gnb.eq(i).parent().addClass('active').siblings().removeClass('active')
+            }  else {
+                $gnb.eq(i).parent().removeClass('on');
+            }
+        }
     });
 
 });
