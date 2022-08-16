290b1cf6e6904db67a8e9bea9388300ff537bd2d
shinchaeyoun <100834190+shinchaeyoun@users.noreply.github.com>
2022-08-16 13:35:10 +0900

    Add files via upload

--- index/function.js
+++ index/function.js
@@ -7,19 +7,15 @@ $(function(){
         arrTop[i] = $('section').eq(i).offset().top;
     };
 
-
     $gnb.click(function(e){
-        //e.preventDefault();
-
         gnbIdx = $gnb.index(this);
-
-        //$(this).parent().addClass('active').siblings().removeClass('active')
 
         $('html,body').stop().animate({
             scrollTop : arrTop[gnbIdx]
         });
     });
 
+    // pc 페이지네이션
     $(window).scroll(function(){
         const scrollTop = $(this).scrollTop();
 
@@ -27,9 +23,27 @@ $(function(){
             if(scrollTop >= arrTop[i] -100 ){
                 $gnb.eq(i).parent().addClass('active').siblings().removeClass('active')
             }  else {
-                $gnb.eq(i).parent().removeClass('on');
+                $gnb.eq(i).parent().removeClass('active');
             }
         }
     });
 
+
+    // 반응형
+    $(window).resize(function(){ 
+        
+        if (window.innerWidth < 768) {  
+            // 메인 페이드슬라이드
+            $('.main_visual .frame').hide();
+            $('.main_visual .frame:first-child').show();
+
+            setInterval(() => {
+                $('.main_visual .frame:first-child').fadeOut().next().fadeIn().end().appendTo('.main_visual');
+
+            }, 3000);
+        } 
+    
+    }).resize();
+    // 반응형 끝 
 });
+
