bba482b2fbcc9c391d722d98b6f6f8e6269770f0
shinchaeyoun <100834190+shinchaeyoun@users.noreply.github.com>
2023-02-20 14:04:42 +0900

    Add files via upload

---
+++ canvas/app.js
@@ -0,0 +1,107 @@
+console.log('canvas load');
+
+// 전역 변수 선언
+let canvas,
+    div,
+    ctx,
+    drawble = false,
+    $url,
+    $picture,
+    $delete;
+
+
+$(function () {
+    
+});
+
+$(window).load(function(){
+    console.log('ready handler');
+
+    // 전역 변수 객체 등록; 캔버스 오브젝트 가져오기;
+    canvas = $("#canvas");
+    div = $(".canvas_container");
+    ctx = canvas[0].getContext("2d");
+
+    $url = $('.url_container');
+    $picture = $('.picture_container');
+    $delete = $('.delete_container');
+
+    canvas[0].width = div.width();
+    canvas[0].height = div.height();
+    ctx.fillStyle = "orange";
+    console.log(ctx);
+    // 이벤트 함수 호출
+
+    init();
+    buttonEvent ();
+})
+
+//이벤트 함수
+function init() {
+    canvas.on('mousedown', draw);
+    canvas.on('mousemove', draw);
+    canvas.on('mouseup', draw);
+    canvas.on('mouseout', draw);
+};
+
+// 화면 조절 함수
+function canvasResize() {
+    canvas[0].width = div.width();
+    canvas[0].height = div.height();
+};
+
+// draw 이벤트 함수
+function draw(e) {
+    switch (e.type) {
+        case "mousedown": {
+            drawble = true;
+            ctx.beginPath();
+            ctx.moveTo(getPosition(e).X, getPosition(e).Y);
+        }
+            break;
+
+        case "mousemove": {
+            if (drawble) {
+                ctx.lineTo(getPosition(e).X, getPosition(e).Y);
+                ctx.stroke();
+            }
+        }
+            break;
+
+        case "mouseup":
+        case "mouseout": {
+            drawble = false;
+            ctx.closePath();
+        }
+            break;
+    }
+};
+
+function getPosition(e) {
+    let x = e.pageX - canvas.offset().left;
+    let y = e.pageY - canvas.offset().top;
+
+    return { X: x, Y: y };
+};
+
+function buttonEvent (){
+    $url.on('click', function (){
+        console.log(canvas[0].toDataURL());
+    });
+
+    $picture.on('click', function (){
+        let link = document.createElement('a');
+
+        link.href = canvas[0].toDataURL('image/png');
+        link.download = 'image.png';
+
+        document.body.appendChild(link);
+        link.click();
+
+        document.body.removeChild(link);
+    })
+
+    $delete.on('click', function (){
+        canvasResize();
+    });
+};
