2ba2bad862e3b7c074b8ce4c4530718650c2a10d
shinchaeyoun <100834190+shinchaeyoun@users.noreply.github.com>
2023-03-22 16:36:16 +0900

    Add files via upload

--- canvas_typeChange/common.js
+++ canvas_typeChange/common.js
@@ -10,7 +10,9 @@ $(function(){
 
 let loadCheck = 0,
     info,type,
-    nowUrl;
+    nowUrl,
+    pagePrev, pageNext,
+    totalPage;
 
 nowUrl = location.href;
 urlNum = Number(nowUrl.split('/')[4].split('.')[0].split('canvas')[1]);
@@ -20,6 +22,13 @@ function complete (){
 
     if(loadCheck == 2){
         info = new pageInfoFn();
+
+        pagePrev = $('.page_controls .prev');
+        pageNext = $('.page_controls .next');
+
+        totalPage = info.canvasType.length;
+
+        console.log(totalPage);
         
         init();
         canvasResize();
@@ -27,6 +36,7 @@ function complete (){
         saveImg();
         buttonEvent();
         page();
+        pageControls();
     };  
 };
 
@@ -34,4 +44,38 @@ function page (){
     nowType = info.canvasType[urlNum - 1];
 
     canvasTypeFn(nowType);
+};
+
+
+
+function pageControls(){
+    pagePrev.on('click', function(){
+        urlNum--;
+        if(urlNum < 1) {
+            urlNum = 1;
+            alert('First page');
+        };
+        pageNum = itostr(urlNum)
+        pageMove(pageNum)
+    });
+
+    pageNext.on('click', function(){
+        urlNum++;
+        if(urlNum > totalPage){
+            urlNum = totalPage;
+            alert('Last page');
+        };
+        pageNum = itostr(urlNum)
+        pageMove (pageNum);
+    });
+};
+
+function pageMove ($pageNum){
+    changeUrl = nowUrl.replace(nowUrl.split('/')[4].split('.')[0].split('canvas')[1],$pageNum);
+    location.href = changeUrl;
+    console.log('');
+};
+
+function itostr ($num){
+    return $num < 10 ? '0'+$num : $num;
 };
