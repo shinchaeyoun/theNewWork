4f4bc85dcb7f50abc576227ae641ad497c567253
shinchaeyoun <100834190+shinchaeyoun@users.noreply.github.com>
2023-03-25 14:28:30 +0900

    Update common.js

--- TheNewWork/js/common.js
+++ TheNewWork/js/common.js
@@ -1,6 +1,26 @@
-$('header').load('inc.html header > div', function(){
-    // 흑백 전환
-    const $body = $("body");
+$(function(){
+  $('header').load('inc.html header > div', function(){
+    complete();
+  });
+
+  $('footer').load('inc.html footer > div ',function(){
+    complete();
+  });
+});
+
+let loadCheck = 0;
+
+function complete(){
+  loadCheck++;
+  
+  if(loadCheck == 2){
+    onoffFn();
+    dateFn();
+    cusorFn();
+  }; // if end
+};
+
+function onoffFn(){
     const $backGround = $(".bg");
     const $red = $(".title a .red");
     const $onoff = $(".onoff");
@@ -64,21 +84,70 @@ $('header').load('inc.html header > div', function(){
       $contactThx.toggleClass('active');
       $footerRed.toggleClass('active');
       $footerSmallLogo.toggleClass('active');
+
+      if($onoffLi.hasClass('active')){
+        localStorage.setItem('saveType','on');
+      } else {
+        localStorage.setItem('saveType','off');
+      };
     });
 
-// top menu show hide
-const $topMenu = $('.top_menu');
-const $headerHei = $('header').height();
+    if(localStorage.getItem('saveType') == 'on'){
+      $onoffLi.addClass('active');
+      $backGround.addClass('active');
+      $red.addClass('active');
+      $mainSubTit.addClass('active');
+      $intTit.addClass('active');
+      $intSubTit.addClass('active');
+      $workTit.addClass('active');
+      $topBg.addClass('active');
+      $topLogo.addClass('active');
+      $workMainTit.addClass('active');
+      $workSpan.addClass('active');
+      $likeTit.addClass('active');
+      $travelTit.addClass('active');
+      $travelTabTit.addClass('active');
+      $flimArrow.addClass('active');
+      $moviesTit.addClass('active');
+      $musicTit.addClass('active');
+      $booksTit.addClass('active');
+      $companyName.addClass('active');
+      $contactTit.addClass('active');
+      $contactBtn.addClass('active');
+      $contactSubTit.addClass('active');
+      $contactThx.addClass('active');
+      $footerRed.addClass('active');
+      $footerSmallLogo.addClass('active');
+    } else {
+      $onoffLi.removeClass('active');
+      $backGround.removeClass('active');
+      $red.removeClass('active');
+      $mainSubTit.removeClass('active');
+      $intTit.removeClass('active');
+      $intSubTit.removeClass('active');
+      $workTit.removeClass('active');
+      $topBg.removeClass('active');
+      $topLogo.removeClass('active');
+      $workMainTit.removeClass('active');
+      $workSpan.removeClass('active');
+      $likeTit.removeClass('active');
+      $travelTit.removeClass('active');
+      $travelTabTit.removeClass('active');
+      $flimArrow.removeClass('active');
+      $moviesTit.removeClass('active');
+      $musicTit.removeClass('active');
+      $booksTit.removeClass('active');
+      $companyName.removeClass('active');
+      $contactTit.removeClass('active');
+      $contactBtn.removeClass('active');
+      $contactSubTit.removeClass('active');
+      $contactThx.removeClass('active');
+      $footerRed.removeClass('active');
+      $footerSmallLogo.removeClass('active');
+    }
+};
 
-$(window).on('scroll',function (){
-  const scrollTop = $(document).scrollTop();
-  if($headerHei < scrollTop) {
-    $topMenu.slideDown(200);
-  } else {
-    $topMenu.slideUp(200);
-  }
-});
-
+function dateFn(){
   // 날짜 시간 분 초 실시간
   var week = new Array('Sunday,', 'Monday,', 'Tuesday,', 'Wednesday,', 'Thursday,', 'Friday,', 'Saturday,'); 
   var month = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'); 
@@ -115,260 +184,127 @@ $(window).on('scroll',function (){
       `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 
       Target_apm.innerText = `${AmPm}`;
-
-  }
+  };
   clock();
   setInterval(clock, 1000);
-
-//cursor strat
-const shuffleArray = arr => arr.sort(() => Math.random() - 0.5);
-const lineEq = (y2, y1, x2, x1, currentVal) => {
-    let m = (y2 - y1) / (x2 - x1); 
-    let b = y1 - m * x1;
-    return m * currentVal + b;
 };
-const lerp = (a, b, n) => (1 - n) * a + n * b;
-const body = document.body;
-const bodyColor = getComputedStyle(body).getPropertyValue('--color-bg').trim() || 'white';
-const getMousePos = (e) => {
-    let posx = 0;
-    let posy = 0;
-    if (!e) e = window.event;
-    if (e.pageX || e.pageY) {
-        posx = e.pageX;
-        posy = e.pageY;
-    }
-    else if (e.clientX || e.clientY) 	{
-        posx = e.clientX + body.scrollLeft + document.documentElement.scrollLeft;
-        posy = e.clientY + body.scrollTop + document.documentElement.scrollTop;
-    }
-    return { x : posx, y : posy }
-}
 
-// Window sizes.
-let winsize;
-const calcWinsize = () => winsize = {width: window.innerWidth, height: window.innerHeight};
-calcWinsize();
-// Recalculate window sizes on resize.
-window.addEventListener('resize', calcWinsize);
-
-// Custom mouse cursor.
-class CursorFx {
-    constructor(el) {
-        this.DOM = {el: el};
-        this.DOM.dot = this.DOM.el.querySelector('.cursor__inner--dot');
-        this.DOM.circle = this.DOM.el.querySelector('.cursor__inner--circle');
-        this.bounds = {dot: this.DOM.dot.getBoundingClientRect(), circle: this.DOM.circle.getBoundingClientRect()};
-        this.scale = 1;
-        this.opacity = 1;
-        this.mousePos = {x:0, y:0};
-        this.lastMousePos = {dot: {x:0, y:0}, circle: {x:0, y:0}};
-        this.lastScale = 1;
-        this.lastOpacity = 1;
-        
-        this.initEvents();
-        requestAnimationFrame(() => this.render());
-    }
-    initEvents() {
-        window.addEventListener('mousemove', ev => this.mousePos = getMousePos(ev));
-    }
-    render() {
-        this.lastMousePos.dot.x = lerp(this.lastMousePos.dot.x, this.mousePos.x - this.bounds.dot.width/2, 1);
-        this.lastMousePos.dot.y = lerp(this.lastMousePos.dot.y, this.mousePos.y - this.bounds.dot.height/2, 1);
-        this.lastMousePos.circle.x = lerp(this.lastMousePos.circle.x, this.mousePos.x - this.bounds.circle.width/2, 0.15);
-        this.lastMousePos.circle.y = lerp(this.lastMousePos.circle.y, this.mousePos.y - this.bounds.circle.height/2, 0.15);
-        this.lastScale = lerp(this.lastScale, this.scale, 0.15);
-        this.lastOpacity = lerp(this.lastOpacity, this.opacity, 0.1);
-        this.DOM.dot.style.transform = `translateX(${(this.lastMousePos.dot.x)}px) translateY(${this.lastMousePos.dot.y}px)`;
-        this.DOM.circle.style.transform = `translateX(${(this.lastMousePos.circle.x)}px) translateY(${this.lastMousePos.circle.y}px) scale(${this.lastScale})`;
-        this.DOM.circle.style.opacity = this.lastOpacity
-        requestAnimationFrame(() => this.render());
-    }
-    enter() {
-        cursor.scale = 3.5;
-        this.DOM.circle.style = "border:none; background-color:rgba(255,255,255,1);mix-blend-mode:difference";
-    }
-    enter2() {
-        this.DOM.dot.style = "opacity:0;"
-        cursor.scale = 0;
-        this.DOM.circle.style = "";
-    }
-    
-    leave() {
-        cursor.scale = 1;
-        this.DOM.dot.style = "background-color:#000";
-        this.DOM.circle.style = "border:1px solid#000; background-color:none;";
-    }
-    click() {
-        this.lastScale = 1;
-        this.lastOpacity = 0;
-    }
-}
-
-const cursor = new CursorFx(document.querySelector('.cursor'));
-
-// Custom cursor chnages state when hovering on elements with 'data-hover'.
-[...document.querySelectorAll('[data-hover]')].forEach((link) => {
-    link.addEventListener('mouseenter', () => cursor.enter() );
-    link.addEventListener('mouseleave', () => cursor.leave() );
-    link.addEventListener('click', () => cursor.click() );
-});
-
-[...document.querySelectorAll('[data-hover2]')].forEach((link) => {
-  link.addEventListener('mouseenter', () => cursor.enter2() );
-  link.addEventListener('mouseleave', () => cursor.leave() );
-  link.addEventListener('click', () => cursor.click() );
-});
-
-
-
-const white_cursor = document.querySelectorAll(".white_cursor");
-const inner_circle = document.querySelector(".cursor__inner--circle");
-const inner_dot = document.querySelector(".cursor__inner--dot");
-
-for(i=0; i<white_cursor.length; i++){
-  white_cursor[i].addEventListener('mouseover',function(){
-    inner_circle.style = "border:1px solid#fff;";
-    inner_dot.style = "background-color:#fff;";
-  })
-  white_cursor[i].addEventListener('mouseleave',function(){
-    inner_circle.style = "border:1px solid#000;";
-    inner_dot.style = "background-color:#000;";
-  })
-}
-//cusor end
-
-})//header inc
-
-
-
-//footer inc
-$('footer').load('inc.html footer > div ',function(){
-    // 흑백 전환
-    const $onoff = $(".onoff");
-    const $footerRed = $('footer .logo .red');
-    const $footerSmallLogo = $('footer .bottom span');
-    
-    $onoff.on('click', function (){
-      $footerRed.toggleClass('active');
-      $footerSmallLogo.toggleClass('active');
-    });
-
+function cusorFn(){
   //cursor strat
-const shuffleArray = arr => arr.sort(() => Math.random() - 0.5);
-const lineEq = (y2, y1, x2, x1, currentVal) => {
-    let m = (y2 - y1) / (x2 - x1); 
-    let b = y1 - m * x1;
-    return m * currentVal + b;
-};
-const lerp = (a, b, n) => (1 - n) * a + n * b;
-const body = document.body;
-const bodyColor = getComputedStyle(body).getPropertyValue('--color-bg').trim() || 'white';
-const getMousePos = (e) => {
-    let posx = 0;
-    let posy = 0;
-    if (!e) e = window.event;
-    if (e.pageX || e.pageY) {
-        posx = e.pageX;
-        posy = e.pageY;
-    }
-    else if (e.clientX || e.clientY) 	{
-        posx = e.clientX + body.scrollLeft + document.documentElement.scrollLeft;
-        posy = e.clientY + body.scrollTop + document.documentElement.scrollTop;
-    }
-    return { x : posx, y : posy }
-}
+  const shuffleArray = arr => arr.sort(() => Math.random() - 0.5);
+  const lineEq = (y2, y1, x2, x1, currentVal) => {
+      let m = (y2 - y1) / (x2 - x1); 
+      let b = y1 - m * x1;
+      return m * currentVal + b;
+  };
+  const lerp = (a, b, n) => (1 - n) * a + n * b;
+  const body = document.body;
+  const bodyColor = getComputedStyle(body).getPropertyValue('--color-bg').trim() || 'white';
+  const getMousePos = (e) => {
+      let posx = 0;
+      let posy = 0;
+      if (!e) e = window.event;
+      if (e.pageX || e.pageY) {
+          posx = e.pageX;
+          posy = e.pageY;
+      }
+      else if (e.clientX || e.clientY) 	{
+          posx = e.clientX + body.scrollLeft + document.documentElement.scrollLeft;
+          posy = e.clientY + body.scrollTop + document.documentElement.scrollTop;
+      }
+      return { x : posx, y : posy }
+  }
 
-// Window sizes.
-let winsize;
-const calcWinsize = () => winsize = {width: window.innerWidth, height: window.innerHeight};
-calcWinsize();
-// Recalculate window sizes on resize.
-window.addEventListener('resize', calcWinsize);
+  // Window sizes.
+  let winsize;
+  const calcWinsize = () => winsize = {width: window.innerWidth, height: window.innerHeight};
+  calcWinsize();
+  // Recalculate window sizes on resize.
+  window.addEventListener('resize', calcWinsize);
 
-// Custom mouse cursor.
-class CursorFx {
-    constructor(el) {
-        this.DOM = {el: el};
-        this.DOM.dot = this.DOM.el.querySelector('.cursor__inner--dot');
-        this.DOM.circle = this.DOM.el.querySelector('.cursor__inner--circle');
-        this.bounds = {dot: this.DOM.dot.getBoundingClientRect(), circle: this.DOM.circle.getBoundingClientRect()};
-        this.scale = 1;
-        this.opacity = 1;
-        this.mousePos = {x:0, y:0};
-        this.lastMousePos = {dot: {x:0, y:0}, circle: {x:0, y:0}};
-        this.lastScale = 1;
-        this.lastOpacity = 1;
-        
-        this.initEvents();
-        requestAnimationFrame(() => this.render());
-    }
-    initEvents() {
-        window.addEventListener('mousemove', ev => this.mousePos = getMousePos(ev));
-    }
-    render() {
-        this.lastMousePos.dot.x = lerp(this.lastMousePos.dot.x, this.mousePos.x - this.bounds.dot.width/2, 1);
-        this.lastMousePos.dot.y = lerp(this.lastMousePos.dot.y, this.mousePos.y - this.bounds.dot.height/2, 1);
-        this.lastMousePos.circle.x = lerp(this.lastMousePos.circle.x, this.mousePos.x - this.bounds.circle.width/2, 0.15);
-        this.lastMousePos.circle.y = lerp(this.lastMousePos.circle.y, this.mousePos.y - this.bounds.circle.height/2, 0.15);
-        this.lastScale = lerp(this.lastScale, this.scale, 0.15);
-        this.lastOpacity = lerp(this.lastOpacity, this.opacity, 0.1);
-        this.DOM.dot.style.transform = `translateX(${(this.lastMousePos.dot.x)}px) translateY(${this.lastMousePos.dot.y}px)`;
-        this.DOM.circle.style.transform = `translateX(${(this.lastMousePos.circle.x)}px) translateY(${this.lastMousePos.circle.y}px) scale(${this.lastScale})`;
-        this.DOM.circle.style.opacity = this.lastOpacity
-        requestAnimationFrame(() => this.render());
-    }
-    enter() {
-        cursor.scale = 3.5;
-        this.DOM.circle.style = "border:none; background-color:rgba(255,255,255,1);mix-blend-mode:difference";
-    }
-    enter2() {
-        this.DOM.dot.style = "opacity:0;"
-        cursor.scale = 0;
-        this.DOM.circle.style = "";
-    }
-    
-    leave() {
-        cursor.scale = 1;
-        this.DOM.dot.style = "background-color:#000";
-        this.DOM.circle.style = "border:1px solid#000; background-color:none;";
-    }
-    click() {
-        this.lastScale = 1;
-        this.lastOpacity = 0;
-    }
-}
+  // Custom mouse cursor.
+  class CursorFx {
+      constructor(el) {
+          this.DOM = {el: el};
+          this.DOM.dot = this.DOM.el.querySelector('.cursor__inner--dot');
+          this.DOM.circle = this.DOM.el.querySelector('.cursor__inner--circle');
+          this.bounds = {dot: this.DOM.dot.getBoundingClientRect(), circle: this.DOM.circle.getBoundingClientRect()};
+          this.scale = 1;
+          this.opacity = 1;
+          this.mousePos = {x:0, y:0};
+          this.lastMousePos = {dot: {x:0, y:0}, circle: {x:0, y:0}};
+          this.lastScale = 1;
+          this.lastOpacity = 1;
+          
+          this.initEvents();
+          requestAnimationFrame(() => this.render());
+      }
+      initEvents() {
+          window.addEventListener('mousemove', ev => this.mousePos = getMousePos(ev));
+      }
+      render() {
+          this.lastMousePos.dot.x = lerp(this.lastMousePos.dot.x, this.mousePos.x - this.bounds.dot.width/2, 1);
+          this.lastMousePos.dot.y = lerp(this.lastMousePos.dot.y, this.mousePos.y - this.bounds.dot.height/2, 1);
+          this.lastMousePos.circle.x = lerp(this.lastMousePos.circle.x, this.mousePos.x - this.bounds.circle.width/2, 0.15);
+          this.lastMousePos.circle.y = lerp(this.lastMousePos.circle.y, this.mousePos.y - this.bounds.circle.height/2, 0.15);
+          this.lastScale = lerp(this.lastScale, this.scale, 0.15);
+          this.lastOpacity = lerp(this.lastOpacity, this.opacity, 0.1);
+          this.DOM.dot.style.transform = `translateX(${(this.lastMousePos.dot.x)}px) translateY(${this.lastMousePos.dot.y}px)`;
+          this.DOM.circle.style.transform = `translateX(${(this.lastMousePos.circle.x)}px) translateY(${this.lastMousePos.circle.y}px) scale(${this.lastScale})`;
+          this.DOM.circle.style.opacity = this.lastOpacity
+          requestAnimationFrame(() => this.render());
+      }
+      enter() {
+          cursor.scale = 3.5;
+          this.DOM.circle.style = "border:none; background-color:rgba(255,255,255,1);mix-blend-mode:difference";
+      }
+      enter2() {
+          this.DOM.dot.style = "opacity:0;"
+          cursor.scale = 0;
+          this.DOM.circle.style = "";
+      }
+      
+      leave() {
+          cursor.scale = 1;
+          this.DOM.dot.style = "background-color:#000";
+          this.DOM.circle.style = "border:1px solid#000; background-color:none;";
+      }
+      click() {
+          this.lastScale = 1;
+          this.lastOpacity = 0;
+      }
+  }
 
-const cursor = new CursorFx(document.querySelector('.cursor'));
+  const cursor = new CursorFx(document.querySelector('.cursor'));
 
-// Custom cursor chnages state when hovering on elements with 'data-hover'.
-[...document.querySelectorAll('[data-hover]')].forEach((link) => {
-    link.addEventListener('mouseenter', () => cursor.enter() );
+  // Custom cursor chnages state when hovering on elements with 'data-hover'.
+  [...document.querySelectorAll('[data-hover]')].forEach((link) => {
+      link.addEventListener('mouseenter', () => cursor.enter() );
+      link.addEventListener('mouseleave', () => cursor.leave() );
+      link.addEventListener('click', () => cursor.click() );
+  });
+
+  [...document.querySelectorAll('[data-hover2]')].forEach((link) => {
+    link.addEventListener('mouseenter', () => cursor.enter2() );
     link.addEventListener('mouseleave', () => cursor.leave() );
     link.addEventListener('click', () => cursor.click() );
-});
-
-[...document.querySelectorAll('[data-hover2]')].forEach((link) => {
-  link.addEventListener('mouseenter', () => cursor.enter2() );
-  link.addEventListener('mouseleave', () => cursor.leave() );
-  link.addEventListener('click', () => cursor.click() );
-});
+  });
 
 
 
-const white_cursor = document.querySelectorAll(".white_cursor");
-const inner_circle = document.querySelector(".cursor__inner--circle");
-const inner_dot = document.querySelector(".cursor__inner--dot");
+  const white_cursor = document.querySelectorAll(".white_cursor");
+  const inner_circle = document.querySelector(".cursor__inner--circle");
+  const inner_dot = document.querySelector(".cursor__inner--dot");
 
-for(i=0; i<white_cursor.length; i++){
-  white_cursor[i].addEventListener('mouseover',function(){
-    inner_circle.style = "border:1px solid#fff;";
-    inner_dot.style = "background-color:#fff;";
-  })
-  white_cursor[i].addEventListener('mouseleave',function(){
-    inner_circle.style = "border:1px solid#000;";
-    inner_dot.style = "background-color:#000;";
-  })
-}
-//cusor end
-})
+  for(i=0; i<white_cursor.length; i++){
+    white_cursor[i].addEventListener('mouseover',function(){
+      inner_circle.style = "border:1px solid#fff;";
+      inner_dot.style = "background-color:#fff;";
+    })
+    white_cursor[i].addEventListener('mouseleave',function(){
+      inner_circle.style = "border:1px solid#000;";
+      inner_dot.style = "background-color:#000;";
+    });
+  };
+  //cusor end
+};
