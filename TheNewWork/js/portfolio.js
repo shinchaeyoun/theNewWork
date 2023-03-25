aa09c0c4f3cd893a52d32dbf50eefa6d6b5a6897
shinchaeyoun <100834190+shinchaeyoun@users.noreply.github.com>
2023-03-25 13:09:46 +0900

    Update portfolio.js

--- TheNewWork/js/portfolio.js
+++ TheNewWork/js/portfolio.js
@@ -1,4 +1,4 @@
- $(function(){
+$(function(){
   const windowHei = window.innerHeight,
         $minusWin = windowHei / 2,
         $port = $('#portfolio'),
@@ -9,107 +9,46 @@
         $txt = $('.text_box'),
         $contentHei = $('.content_wrap').offset().top;
         
-        let nowIdx = null;
-        const top = [];
+  let nowIdx,
+      top = [];
 
-        for(let i=0;i<$content.length;i++){
-          top[i] = $content.eq(i).offset().top;
-        }
+  for(let i=0;i<$content.length;i++){
+    top[i] = $content.eq(i).offset().top;
+  };
 
-        nowIdx = $content.index(this);
+  nowIdx = $content.index(this);
 
-        console.log(nowIdx);
-        
-        $(window).on('load',function(){
-          for(let i=0;i<top.length;i++){
+  $(window).on('load',function(){
+    for(let i=0;i<top.length;i++){
+      if(window.scrollY > top[i] - $minusWin){
+        $mainTit.eq(i).addClass('on');
+        $subTit.eq(i).addClass('on');
+        $img.eq(i).addClass('on');
+        $txt.eq(i).addClass('on');
+      } else if (window.scrollY < top[i] - $minusWin){
+        $mainTit.eq(i).removeClass('on');
+        $subTit.eq(i).removeClass('on');
+        $img.eq(i).removeClass('on');
+        $txt.eq(i).removeClass('on');
+      }
+    };
+  });
 
-            if(window.scrollY > top[0] - $minusWin){
-              $mainTit.eq(0).addClass('on');
-              $subTit.eq(0).addClass('on');
-              $img.eq(0).addClass('on');
-              $txt.eq(0).addClass('on');
-            } else if (window.scrollY < top[0] - $minusWin){
-              $mainTit.eq(0).removeClass('on');
-              $subTit.eq(0).removeClass('on');
-              $img.eq(0).removeClass('on');
-              $txt.eq(0).removeClass('on');
-            }
-          };
-        });
+  $(window).on('scroll',function(){
+    nowIdx = $content.index(this);
 
-        $(window).on('scroll',function(){
-          nowIdx = $content.index(this);
-
-          for(let i=0;i<top.length;i++){
-            if(window.scrollY > top[1] - $minusWin){
-              $mainTit.eq(1).addClass('on');
-              $subTit.eq(1).addClass('on');
-              $img.eq(1).addClass('on');
-              $txt.eq(1).addClass('on');
-            } else if (window.scrollY < top[1] - $minusWin){
-              $mainTit.eq(1).removeClass('on');
-              $subTit.eq(1).removeClass('on');
-              $img.eq(1).removeClass('on');
-              $txt.eq(1).removeClass('on');
-            }
-            if(window.scrollY > top[2] - $minusWin){
-              $mainTit.eq(2).addClass('on');
-              $subTit.eq(2).addClass('on');
-              $img.eq(2).addClass('on');
-              $txt.eq(2).addClass('on');
-            } else if (window.scrollY < top[2] - $minusWin){
-              $mainTit.eq(2).removeClass('on');
-              $subTit.eq(2).removeClass('on');
-              $img.eq(2).removeClass('on');
-              $txt.eq(2).removeClass('on');
-            }
-            if(window.scrollY > top[3] - $minusWin){
-              $mainTit.eq(3).addClass('on');
-              $subTit.eq(3).addClass('on');
-              $img.eq(3).addClass('on');
-              $txt.eq(3).addClass('on');
-            } else if (window.scrollY < top[3] - $minusWin){
-              $mainTit.eq(3).removeClass('on');
-              $subTit.eq(3).removeClass('on');
-              $img.eq(3).removeClass('on');
-              $txt.eq(3).removeClass('on');
-            }
-            if(window.scrollY > top[4] - $minusWin){
-              $mainTit.eq(4).addClass('on');
-              $subTit.eq(4).addClass('on');
-              $img.eq(4).addClass('on');
-              $txt.eq(4).addClass('on');
-            } else if (window.scrollY < top[4] - $minusWin){
-              $mainTit.eq(4).removeClass('on');
-              $subTit.eq(4).removeClass('on');
-              $img.eq(4).removeClass('on');
-              $txt.eq(4).removeClass('on');
-            }
-            if(window.scrollY > top[5] - $minusWin){
-              $mainTit.eq(5).addClass('on');
-              $subTit.eq(5).addClass('on');
-              $img.eq(5).addClass('on');
-              $txt.eq(5).addClass('on');
-            } else if (window.scrollY < top[5] - $minusWin){
-              $mainTit.eq(5).removeClass('on');
-              $subTit.eq(5).removeClass('on');
-              $img.eq(5).removeClass('on');
-              $txt.eq(5).removeClass('on');
-            }
-          }
-
-          // if(window.scrollY > scrollTop){
-          //   $mainTit.addClass('on');
-          //   $subTit.addClass('on');
-          //   $img.addClass('on');
-          //   $txt.addClass('on');
-          // } else if (window.scrollY < $contentHei - $minusWin){
-          //   $mainTit.removeClass('on');
-          //   $subTit.removeClass('on');
-          //   $img.removeClass('on');
-          //   $txt.removeClass('on');
-          // }
-
-          
-        });
+    for(let i=1;i<top.length;i++){
+      if(window.scrollY > top[i] - $minusWin){
+        $mainTit.eq(i).addClass('on');
+        $subTit.eq(i).addClass('on');
+        $img.eq(i).addClass('on');
+        $txt.eq(i).addClass('on');
+      } else if (window.scrollY < top[i] - $minusWin){
+        $mainTit.eq(i).removeClass('on');
+        $subTit.eq(i).removeClass('on');
+        $img.eq(i).removeClass('on');
+        $txt.eq(i).removeClass('on');
+      };
+    };
+  });
 });
