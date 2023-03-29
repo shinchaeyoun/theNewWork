a6bd0192a9b2fe3ac0ac473aa139489e668b225b
shinchaeyoun <100834190+shinchaeyoun@users.noreply.github.com>
2023-03-29 15:04:25 +0900

    Update portfolio.js

--- TheNewWork/js/portfolio.js
+++ TheNewWork/js/portfolio.js
@@ -1,54 +1,31 @@
 $(function(){
-  const windowHei = window.innerHeight,
-        $minusWin = windowHei / 2,
-        $port = $('#portfolio'),
-        $content = $('.content_wrap'),
-        $mainTit = $('.main_tit'),
-        $subTit = $('.sub_tit'),
-        $img = $('.img_box'),
-        $txt = $('.text_box'),
-        $contentHei = $('.content_wrap').offset().top;
-        
-  let nowIdx,
-      top = [];
-
-  for(let i=0;i<$content.length;i++){
-    top[i] = $content.eq(i).offset().top;
-  };
-
-  nowIdx = $content.index(this);
-
-  $(window).on('load',function(){
-    for(let i=0;i<top.length;i++){
-      if(window.scrollY > top[i] - $minusWin){
-        $mainTit.eq(i).addClass('on');
-        $subTit.eq(i).addClass('on');
-        $img.eq(i).addClass('on');
-        $txt.eq(i).addClass('on');
-      } else if (window.scrollY < top[i] - $minusWin){
-        $mainTit.eq(i).removeClass('on');
-        $subTit.eq(i).removeClass('on');
-        $img.eq(i).removeClass('on');
-        $txt.eq(i).removeClass('on');
-      }
+    const windowHei = window.innerHeight,
+          $minusWin = windowHei / 2,
+          $content = $('.content_wrap'),
+          $mainTit = $('.main_tit'),
+          $subTit = $('.sub_tit'),
+          $img = $('.img_box'),
+          $txt = $('.text_box');
+          
+    let top = [];
+  
+    for(let i=0;i<$content.length;i++){
+      top[i] = $content.eq(i).offset().top;
     };
-  });
-
-  $(window).on('scroll',function(){
-    nowIdx = $content.index(this);
-
-    for(let i=1;i<top.length;i++){
-      if(window.scrollY > top[i] - $minusWin){
-        $mainTit.eq(i).addClass('on');
-        $subTit.eq(i).addClass('on');
-        $img.eq(i).addClass('on');
-        $txt.eq(i).addClass('on');
-      } else if (window.scrollY < top[i] - $minusWin){
-        $mainTit.eq(i).removeClass('on');
-        $subTit.eq(i).removeClass('on');
-        $img.eq(i).removeClass('on');
-        $txt.eq(i).removeClass('on');
+  
+    $(window).on('load scroll',function(){
+      for(let i=0;i<top.length;i++){
+        if(window.scrollY > top[i] - $minusWin){
+          $mainTit.eq(i).addClass('on');
+          $subTit.eq(i).addClass('on');
+          $img.eq(i).addClass('on');
+          $txt.eq(i).addClass('on');
+        } else if (window.scrollY < top[i] - $minusWin){
+          $mainTit.eq(i).removeClass('on');
+          $subTit.eq(i).removeClass('on');
+          $img.eq(i).removeClass('on');
+          $txt.eq(i).removeClass('on');
+        };
       };
-    };
+    });
   });
-});
