d3a01529fb8031b9dacfa5091d00770412a6d0ae
shinchaeyoun <jun29182877@gamil.com>
2023-12-08 11:08:34 +0900

    update

--- the-new/src/routes/PortfolioPage.js
+++ the-new/src/routes/PortfolioPage.js
@@ -7,6 +7,51 @@ import S from './../styles/GlobalBlock';
 import portfolioData from './../Data/PortfolioData';
 import ScrollFade from '../ScrollFadeAnimation';
 
+
+const SubTitle = styled.div`
+  margin-bottom: 20px;
+  font-size: 14px;
+  color: ${({theme}) => theme.colors.subGray};
+  span {
+    display: block;
+    font-size: 11px;
+  }
+`
+const Title = styled.div`
+  margin-top: 10px;
+  margin-bottom: 5px;
+  font-size: 30px;
+  font-weight: 600;
+  font-family: ${({ theme }) => theme.fonts.nanum};
+`
+const TitleBox = styled.div`
+  padding: 8px 0 20px;
+`
+const FullImg = styled.img`
+  width: 100%;
+`
+const ImgBlock = styled.div`
+  margin-bottom: 30px;
+  width: 100%;
+  height: 550px;
+  overflow-y: hidden;
+  border: 1px solid #000;
+`
+const BoldText = styled.span`
+  display: inline-block;
+  margin-right: 5px;
+  margin-left: 10px;
+  font-size: 12px;
+  font-weight: 800;
+  text-decoration: underline;
+  &:first-child {
+    margin-left: 0px;
+  }
+`
+const TextBlock = styled(S.TextBox)`
+  line-height: 1.3;
+`
+
 function PortfolioPage() {
   commonFn.MoveToContentTopFn();
   
@@ -36,7 +81,7 @@ function PortfolioPage() {
 
                 <ScrollFade.Item $type={true} $index={index} $activeIdx={activeIdx} $setActiveIdx={setActiveIdx} $delay='.9s'>
                   <ImgBlock>
-                    <S.ScrollCustom $hei='500px'>
+                    <S.ScrollCustom $hei='550px'>
                       <FullImg src={item.fullImg}/>
                     </S.ScrollCustom>
                   </ImgBlock>
@@ -60,48 +105,6 @@ function PortfolioPage() {
 }
 
 
-const SubTitle = styled.div`
-  margin-bottom: 20px;
-  font-size: 14px;
-  color: ${({theme}) => theme.colors.subGray};
-  span {
-    display: block;
-    font-size: 11px;
-  }
-`
-const Title = styled.div`
-  margin-top: 10px;
-  margin-bottom: 5px;
-  font-size: 30px;
-  font-weight: 600;
-  font-family: ${({ theme }) => theme.fonts.nanum};
-`
-const TitleBox = styled.div`
-  padding: 8px 0 20px;
-`
-const FullImg = styled.img`
-  width: 100%;
-`
-const ImgBlock = styled.div`
-  margin-bottom: 30px;
-  width: 100%;
-  height: 600px;
-  overflow-y: hidden;
-  border: 1px solid #000;
-`
-const BoldText = styled.span`
-  display: inline-block;
-  margin-right: 5px;
-  margin-left: 10px;
-  font-size: 12px;
-  font-weight: 800;
-  text-decoration: underline;
-  &:first-child {
-    margin-left: 0px;
-  }
-`
-const TextBlock = styled(S.TextBox)`
-  line-height: 1.3;
-`
+
 
 export default PortfolioPage;
